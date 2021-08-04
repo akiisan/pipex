7a89c05a924c600e9963cfa887564f1704814fad
Rayane SABOUNDJI <rasaboun@e1r10p17.42.fr>
2021-08-04 21:06:47 +0200

    PUSH

--- main.c
+++ main.c
@@ -8,16 +8,56 @@
 #include <sys/stat.h>
 #include "Libft/libft.h"
 
-int getpath(char **env)
+char **ft_getenv(char **env)
 {
   int i;
+  int m;
+  char *allpath;
+  char **spath;
+
+  allpath = NULL;
+  m = 0;
+  i = 0;
+  spath = NULL;
+  while (env[i] != NULL && ft_strncmp("PATH",env[i],4) != 0)
+  {
+    i++;
+  }
+  if (ft_strncmp("PATH",env[i],4) == 0)
+  {
+    m = ft_strlen(env[i]);
+    allpath = ft_substr(env[i],5,m);
+    spath = ft_split(allpath,':');
+    return (spath);
+  }
+  return (NULL);
+}
+
+void ft_execv(char **path,char **envp,char **argv)
+{
+  int i;
+  char *rpath;
+  char *tmp;
+
+  rpath = NULL;
+  tmp = NULL;
+
 
   i = 0;
-
-  while (env[i] && ft_strncmp("PATH",env[i],4) != 0)
-
-
+  while (path[i] != NULL)
+  {
+    tmp = ft_strjoin(path[i],"/");
+    rpath = ft_strjoin(tmp,argv[0]);
+    free(tmp);
+    if (access(rpath, X_OK) == 0)
+    {
+      execve(rpath,argv,envp);
+    }
+    i++;
+  }
 }
+
+
 
 int main(int argc, char **argv, char **envp)
 {
@@ -25,10 +65,22 @@ int main(int argc, char **argv, char **envp)
   int out;
   int fd[2];
   pid_t child;
+  char **cmd;
+  char **cmdd;  
+  char **path;
 
+  path = NULL;
+  cmdd = NULL;
+  cmd = NULL;
   in = 0;
   out = 0;
   
+if ((path =ft_getenv(envp)) == NULL)
+  {
+    write(1,"error env var",14);
+    exit(0);
+  }
+
   if ((in = open(argv[1],O_RDONLY)) >= 1)
     write(1,"SUCCESS OPEN IN FILE\n",22);
   else
@@ -50,26 +102,26 @@ int main(int argc, char **argv, char **envp)
     dup2(in,STDIN_FILENO);
     dup2(fd[1],STDOUT_FILENO);
     close(fd[1]);
-    execlp("cat", "cat", (char *)0);
+    cmdd = ft_split(argv[2],' ');//exec after
+    ft_execv(path,envp,cmdd);
   }
   else // Child
   {
     if ((out = open(argv[4],O_WRONLY | O_CREAT,0777)) <= 1)
-      write(1,"ERROR",6);
+      write(1,"ERROR Create file",6);
     close(fd[1]);
     dup2(fd[0],0);
     dup2(out,1);
     close(fd[0]);
+    cmd = ft_split(argv[3],' '); //exec after
+    
+    ft_execv(path,envp,cmd);
 
-    execlp("wc", "wc", (char *)0);
+
+
+
+
   }
-
-
-
-
-
-
-
 
 
 
